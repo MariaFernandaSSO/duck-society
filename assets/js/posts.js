eb60ef41c4bc62bac05682b08623adf9eb18c9da
Maria Fernanda Oliveira <mariafernandasso281@gmail.com>
2026-06-24 01:49:31 -0300

    chore: remove test post, sync posts.js

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -9,14 +9,4 @@ const postsData = [
     excerpt: "Duck Society é um site inspirado em blogs e portfólios que fui observando de outros desenvolvedores ao longo do tempo.",
     excerptEn: "Duck Society is a website inspired by blogs and portfolios I've come across from other developers over the years."
   },
-  {
-    slug: "teste",
-    title: "teste",
-    titleEn: "testeenglish",
-    date: "24 jun 2026",
-    dateFilter: "2026-06",
-    tag: "teste1",
-    excerpt: "conteúdo pt br",
-    excerptEn: "content en"
-  },
 ]
